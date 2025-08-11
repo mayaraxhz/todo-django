5b7d1e06f69b3017506bb2c72964f5e39fcfab1b
mayaraxhz <mayaraxhz@gmail.com>
2025-08-11 00:05:58 -0300

    comentario

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import Tarefa
+
+# Listar todas as tarefas
+def listar_tarefas(request):
+    tarefas = Tarefa.objects.all().order_by('-data_criacao')
+    return render(request, 'tarefas/listar_tarefas.html', {'tarefas': tarefas})
+
+# Visualizar detalhes de uma tarefa
+def visualizar_tarefa(request, pk):
+    tarefa = get_object_or_404(Tarefa, pk=pk)
+    return render(request, 'tarefas/visualizar_tarefa.html', {'tarefa': tarefa})
--- todo_projeto/urls.py
+++ todo_projeto/urls.py
@@ -19,4 +19,8 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+     path('', views.listar_tarefas, name='listar_tarefas'),
+
+    path('<int:pk>/', views.visualizar_tarefa, name='visualizar_tarefa'),
 ]
